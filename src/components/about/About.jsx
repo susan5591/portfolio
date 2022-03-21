5adb7a66860923d1baea38a7f2d09e97c2637ecf
Susan Dangol <49741990+susan5591@users.noreply.github.com>
2022-03-21 13:16:57 +0545

    about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
-import './about.css'
+import React from "react";
+import "./about.css";
+import ME from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
   return (
-    <section id='about'>about</section>
-  )
-}
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about_container">
+        <div className="about_me">
+          <img src={ME} alt="about image" />
+        </div>
+        <div className="about_content">
+          <div className="about_cards">
+            <article className="about_card">
+              <FaAward className="about_icon" />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+            <article className="about_card">
+              <FiUsers className="about_icon" />
+              <h5>Clients</h5>
+              <small>2 Worldwide</small>
+            </article>
+            <article className="about_card">
+              <VscFolderLibrary className="about_icon" />
+              <h5>Projects</h5>
+              <small>10+ Projects</small>
+            </article>            
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor,
+            quos, cumque, omnis natus corrupti facilis cupiditate suscipit
+            accusamus obcaecati maxime modi! Aliquid distinctio repellendus
+            nulla ex sequi incidunt repudiandae architecto!
+          </p>
+          <a href="#contact" className="btn btn-primary">Let's Talk</a>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default About
+export default About;
