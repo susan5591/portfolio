96a6160f1cec02a8a0e985a541539fc315440cef
Susan Dangol <49741990+susan5591@users.noreply.github.com>
2022-03-28 21:25:10 +0545

    responsive in small device

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -5,7 +5,7 @@ const CTA = () => {
   return (
     <div className="cta">
       <a href={CV} download className="btn">
-        Download    s  CV
+        Download  CV
       </a>
       <a href="#contact" className="btn btn-primary">
         Let's Talk
